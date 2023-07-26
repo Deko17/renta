35ca70ec1892d935d5394c1262e8877eaeadeb60
Daryn Kenessov <57325957+Deko17@users.noreply.github.com>
2023-07-27 01:41:29 +0600

    Update HeaderFilter.tsx

--- src/containers/PageHome/HeaderFilter.tsx
+++ src/containers/PageHome/HeaderFilter.tsx
@@ -6,58 +6,58 @@ import ButtonSecondary from "shared/Button/ButtonSecondary";
 import { ReactNode } from "react";
 
 export interface HeaderFilterProps {
-  tabActive: string;
-  tabs: string[];
-  heading: ReactNode;
-  subHeading?: ReactNode;
-  onClickTab: (item: string) => void;
+	tabActive: string;
+	tabs: string[];
+	heading: ReactNode;
+	subHeading?: ReactNode;
+	onClickTab: (item: string) => void;
 }
 
 const HeaderFilter: FC<HeaderFilterProps> = ({
-  tabActive,
-  tabs,
-  subHeading = "",
-  heading = "🎈 Latest Articles",
-  onClickTab,
+	tabActive,
+	tabs,
+	subHeading = "",
+	heading = "🎈 Latest Articles",
+	onClickTab,
 }) => {
-  const [tabActiveState, setTabActiveState] = useState(tabActive);
+	const [tabActiveState, setTabActiveState] = useState(tabActive);
 
-  useEffect(() => {
-    setTabActiveState(tabActive);
-  }, [tabActive]);
+	useEffect(() => {
+		setTabActiveState(tabActive);
+	}, [tabActive]);
 
-  const handleClickTab = (item: string) => {
-    onClickTab && onClickTab(item);
-    setTabActiveState(item);
-  };
+	const handleClickTab = (item: string) => {
+		onClickTab && onClickTab(item);
+		setTabActiveState(item);
+	};
 
-  return (
-    <div className="flex flex-col mb-8 relative">
-      <Heading desc={subHeading}>{heading}</Heading>
-      <div className="flex items-center justify-between">
-        <Nav
-          className="sm:space-x-2"
-          containerClassName="relative flex w-full overflow-x-auto text-sm md:text-base hiddenScrollbar"
-        >
-          {tabs.map((item, index) => (
-            <NavItem
-              key={index}
-              isActive={tabActiveState === item}
-              onClick={() => handleClickTab(item)}
-            >
-              {item}
-            </NavItem>
-          ))}
-        </Nav>
-        <span className="hidden sm:block flex-shrink-0">
-          <ButtonSecondary className="!leading-none">
+	return (
+		<div className="flex flex-col mb-8 relative">
+			<Heading desc={subHeading}>{heading}</Heading>
+			<div className="flex items-center justify-between">
+				<Nav
+					className="sm:space-x-2"
+					containerClassName="relative flex w-full overflow-x-auto text-sm md:text-base hiddenScrollbar"
+				>
+					{tabs.map((item, index) => (
+						<NavItem
+							key={index}
+							isActive={tabActiveState === item}
+							onClick={() => handleClickTab(item)}
+						>
+							{item}
+						</NavItem>
+					))}
+				</Nav>
+				<span className="hidden sm:block flex-shrink-0">
+					{/* <ButtonSecondary className="!leading-none">
             <span>View all</span>
             <i className="ml-3 las la-arrow-right text-xl"></i>
-          </ButtonSecondary>
-        </span>
-      </div>
-    </div>
-  );
+          </ButtonSecondary> */}
+				</span>
+			</div>
+		</div>
+	);
 };
 
 export default HeaderFilter;
