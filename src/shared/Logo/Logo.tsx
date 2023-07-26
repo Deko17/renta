f12b7bbb8ff5bd32f4f9e2ea6c95851c85596194
Daryn Kenessov <57325957+Deko17@users.noreply.github.com>
2023-07-27 01:44:18 +0600

    Update Logo.tsx

--- src/shared/Logo/Logo.tsx
+++ src/shared/Logo/Logo.tsx
@@ -6,44 +6,47 @@ import LogoSvgLight from "./LogoSvgLight";
 import LogoSvg from "./LogoSvg";
 
 export interface LogoProps {
-  img?: string;
-  imgLight?: string;
-  className?: string;
+	img?: string;
+	imgLight?: string;
+	className?: string;
 }
 
 const Logo: React.FC<LogoProps> = ({
-  img = logoImg,
-  imgLight = logoLightImg,
-  className = "w-24",
+	img = logoImg,
+	imgLight = logoLightImg,
+	className = "w-24",
 }) => {
-  return (
-    <Link
-      to="/"
-      className={`ttnc-logo inline-block text-primary-6000 focus:outline-none focus:ring-0 ${className}`}
-    >
-      <LogoSvgLight />
-      <LogoSvg />
+	return (
+		<Link
+			to="/"
+			className={`ttnc-logo inline-block text-primary-6000 focus:outline-none focus:ring-0 ${className}`}
+		>
+			{/* <LogoSvgLight />
+			<LogoSvg /> */}
 
-      {/* THIS USE FOR MY CLIENT */}
-      {/* PLEASE UN COMMENT BELLOW CODE AND USE IT */}
-      {/* {img ? (
-        <img
-          className={`block max-h-12 ${imgLight ? "dark:hidden" : ""}`}
-          src={img}
-          alt="Logo"
-        />
-      ) : (
-        "Logo Here"
-      )}
-      {imgLight && (
-        <img
-          className="hidden max-h-12 dark:block"
-          src={imgLight}
-          alt="Logo-Light"
-        />
-      )} */}
-    </Link>
-  );
+			{/* THIS USE FOR MY CLIENT */}
+			{/* PLEASE UN COMMENT BELLOW CODE AND USE IT */}
+			{/* {img ? (
+				<img
+					className={`block max-h-12 ${imgLight ? "dark:hidden" : ""}`}
+					src={img}
+					alt="Logo"
+				/>
+			) : (
+				"Logo Here"
+			)}
+			{imgLight && (
+				<img
+					className="hidden max-h-12 dark:block"
+					src={imgLight}
+					alt="Logo-Light"
+				/>
+			)} */}
+			<div className="relative z-10 hidden md:flex flex-1 text-xl font-serif font-bold">
+				RENTA
+			</div>
+		</Link>
+	);
 };
 
 export default Logo;
