f9286bdecc9a144b24dd6f925ad47580f4146112
Daryn Kenessov <57325957+Deko17@users.noreply.github.com>
2023-07-27 01:43:00 +0600

    Update SectionGridFeatureProperty.tsx

--- src/containers/PageHome/SectionGridFeatureProperty.tsx
+++ src/containers/PageHome/SectionGridFeatureProperty.tsx
@@ -9,45 +9,45 @@ import PropertyCardH from "components/PropertyCardH/PropertyCardH";
 const DEMO_DATA: StayDataType[] = DEMO_STAY_LISTINGS.filter((_, i) => i < 8);
 //
 export interface SectionGridFeaturePropertyProps {
-  stayListings?: StayDataType[];
-  gridClass?: string;
-  heading?: ReactNode;
-  subHeading?: ReactNode;
-  headingIsCenter?: boolean;
-  tabs?: string[];
+	stayListings?: StayDataType[];
+	gridClass?: string;
+	heading?: ReactNode;
+	subHeading?: ReactNode;
+	headingIsCenter?: boolean;
+	tabs?: string[];
 }
 
 const SectionGridFeatureProperty: FC<SectionGridFeaturePropertyProps> = ({
-  stayListings = DEMO_DATA,
-  gridClass = "",
-  heading = "Featured places to stay",
-  subHeading = "Popular places to stay that Chisfis recommends for you",
-  headingIsCenter,
-  tabs = ["New York", "Tokyo", "Paris", "London"],
+	stayListings = DEMO_DATA,
+	gridClass = "",
+	heading = "Наши квартиры",
+	// subHeading = "Popular places to stay that Chisfis recommends for you",
+	headingIsCenter,
+	tabs = ["Стандарт", "Комфорт", "Бизнес", "Бизнес+"],
 }) => {
-  const renderCard = (stay: StayDataType, index: number) => {
-    return <PropertyCardH key={index} className="h-full" data={stay} />;
-  };
+	const renderCard = (stay: StayDataType, index: number) => {
+		return <PropertyCardH key={index} className="h-full" data={stay} />;
+	};
 
-  return (
-    <div className="nc-SectionGridFeatureProperty relative">
-      <HeaderFilter
-        tabActive={"New York"}
-        subHeading={subHeading}
-        tabs={tabs}
-        heading={heading}
-        onClickTab={() => {}}
-      />
-      <div
-        className={`grid gap-6 md:gap-8 grid-cols-1 sm:grid-cols-1 xl:grid-cols-2 ${gridClass}`}
-      >
-        {DEMO_DATA.map(renderCard)}
-      </div>
-      <div className="flex mt-16 justify-center items-center">
-        <ButtonPrimary loading>Show me more</ButtonPrimary>
-      </div>
-    </div>
-  );
+	return (
+		<div className="nc-SectionGridFeatureProperty relative">
+			<HeaderFilter
+				tabActive={"Стандарт"}
+				// subHeading={subHeading}
+				tabs={tabs}
+				heading={heading}
+				onClickTab={() => { }}
+			/>
+			<div
+				className={`grid gap-6 md:gap-8 grid-cols-1 sm:grid-cols-1 xl:grid-cols-2 ${gridClass}`}
+			>
+				{DEMO_DATA.map(renderCard)}
+			</div>
+			<div className="flex mt-16 justify-center items-center">
+				<ButtonPrimary>Показать Больше</ButtonPrimary>
+			</div>
+		</div>
+	);
 };
 
 export default SectionGridFeatureProperty;
